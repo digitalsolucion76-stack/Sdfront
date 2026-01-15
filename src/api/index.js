e5c75d3a0892932574e1824024af6cf76e76ba9f
EchauriMu <edechaurimu@ittepic.edu.mx>
2026-01-14 22:05:57 -0700

    V6

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:82/api/v1',
+  baseURL: 'https://sdback.onrender.com/api/v1',
   withCredentials: true, // This is important for sending cookies
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default apiClient;
